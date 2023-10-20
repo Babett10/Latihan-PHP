41af564169ae9b9d6f443df6092a785da23785b4
Albert Radja Sihite <113775702+Babett10@users.noreply.github.com>
2023-10-20 20:03:35 +0700

    update tugas sesi 27

---
+++ sesi27.php
@@ -0,0 +1,39 @@
+<?php
+//Tugas 1 
+    for ($i=0; $i < 9 ; $i++) {
+        for ($j=0; $j <= $i ; $j++) { 
+            echo "$j";
+        } 
+        echo "<br>";
+    }
+    echo "<br";
+?>
+
+<!-- Tugas 2 -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas</title>
+</head>
+<body>
+    <table border="1">
+        <tr style="background-color: cyan; align-items: center;">
+            <td>No</td>
+            <td>Nama</td>
+            <td>Kelas</td>
+        <tr>
+
+        <?php
+        
+
+
+        ?>
+
+
+    </table>
+    
+</body>
+</html>
+
